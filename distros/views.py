c2591690e5cc303056f9282d8e2227357cc16f54
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-04-04 12:03:49 -0600

    added dictionary of dummy data for list view and logic for displaying said data

--- distros/views.py
+++ distros/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+distroSites = [
+  {
+  'site': 'Church of Rufus',
+  'address': '123 Elm St',
+  'info': 'COR Info',
+  'last_updated': '11 Mar 2019'
+  },
+  {
+  'site': 'Food Not Bombs Denver',
+  'address': '123 14th Ave',
+  'info': 'FNB Info',
+  'last_updated': '13 Mar 2019'
+  }
+]
+
 def home(request):
-  return render(request, 'distros/home.html')
+  context = {
+    'distroSites': distroSites
+  }
+  return render(request, 'distros/home.html', context)
 
 def detail(request):
   return render(request, 'distros/detail.html')
