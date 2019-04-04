8304c43c657ea4fc7c26292066e2299f5ee4f6ee
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-04-04 13:17:47 -0600

    added temp navigation

--- food_stand/settings.py
+++ food_stand/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'distros.apps.DistrosConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
